dad407038bd914e9225a8021096ffbfb720e35f8
Wolfgang Mathurin <wmathurin@salesforce.com>
2014-05-29 10:18:43 -0700

    Changed update.sh to expect sdk's in directories next to SalesforceMobileSDK-CordovaPlugin instead of pulling them with bower No longer depending on SalesforceMobileSDK-iOS-*, only depending on the three main repos (iOS, Android and Shared).

--- tools/update.sh
+++ tools/update.sh
@@ -1,96 +1,147 @@
 #!/bin/bash
 
+# Helper functions
+copy_and_fix ()
+{
+    echo "* Fixing and copying $1 to $2 directory"
+    find tmp -name $1 | xargs sed 's/\#import\ \<Salesforce.*\/\(.*\)\>/#import "\1"/' > src/ios/$2/$1
+}
+
+copy_lib ()
+{
+    echo "* Copying $1"
+    find tmp -name $1 -exec cp {} src/ios/frameworks/ \;
+}
+
+export ANDROID_SDK=../SalesforceMobileSDK-Android
+export IOS_SDK=../SalesforceMobileSDK-iOS
+export SHARED_SDK=../SalesforceMobileSDK-Shared
+
+echo "*** Looking for tools and sdks"
 if [ ! -d "tools" ]
 then
     echo "You must run this tool from the root directory of your repo clone"
+    exit 0
 else
-    echo "*** Fetching latest ios, android and shared repos ***"
-    bower install ./tools
+    echo "Found tools"
+fi 
 
-    echo "*** Creating directories ***"
-    echo "Creating tmp directory"
-    mkdir -p tmp
-    echo "Creating android directories"
-    mkdir -p src/android/native
-    mkdir -p src/android/hybrid
-    mkdir -p src/android/assets
-    echo "Creating ios directories"
-    mkdir -p src/ios/headers
-    mkdir -p src/ios/frameworks
-    mkdir -p src/ios/classes
-    mkdir -p src/ios/resources
-
-    echo "*** Android ***"
-    echo "Copying SalesforceSDK library out of bower_components"
-    cp -r bower_components/mobilesdk-android/native/SalesforceSDK src/android/native/
-    echo "Copying SmartStore library out of bower_components"
-    cp -r bower_components/mobilesdk-android/hybrid/SmartStore src/android/hybrid/
-    echo "Copying icu461.zip out of bower_components"
-    cp bower_components/mobilesdk-android/external/sqlcipher/assets/icudt46l.zip src/android/assets/
-    echo "Copying sqlcipher libs out of bower_components"
-    cp -r bower_components/mobilesdk-android/external/sqlcipher/libs/* src/android/hybrid/SmartStore/libs/    
-
-    echo "*** iOS ***"
-    echo "Copying SalesforceHybridSDK library out of bower_components"    
-    unzip bower_components/mobilesdk-ios-distribution/SalesforceHybridSDK-Debug.zip -d tmp
-    echo "Copying SalesforceOAuth library out of bower_components"    
-    unzip bower_components/mobilesdk-ios-distribution/SalesforceOAuth-Debug.zip -d tmp
-    echo "Copying SalesforceSDKCore library out of bower_components"    
-    unzip bower_components/mobilesdk-ios-distribution/SalesforceSDKCore-Debug.zip -d tmp
-    echo "Copying SalesforceSecurity library out of bower_components"    
-    unzip bower_components/mobilesdk-ios-distribution/SalesforceSecurity-Debug.zip -d tmp
-    echo "Copying SalesforceCommonUtils library out of bower_components"    
-    cp -r bower_components/mobilesdk-ios-dependencies/SalesforceCommonUtils  tmp
-    echo "Copying openssl library out of bower_components"    
-    cp -r bower_components/mobilesdk-ios-dependencies/openssl  tmp
-    echo "Copying sqlcipher library out of bower_components"    
-    cp -r bower_components/mobilesdk-ios-dependencies/sqlcipher  tmp
-    echo "Copying and fixing needed headers to src/ios/headers"
-    copy_and_fix_header ()
-    {
-        echo "* Fixing and copying $1"
-        find tmp -name $1 | xargs sed 's/\#import\ \<Salesforce.*\/\(.*\)\>/#import "\1"/' > src/ios/headers/$1
-    }
-    copy_and_fix_header SFAuthenticationManager.h
-    copy_and_fix_header SFCommunityData.h
-    copy_and_fix_header SFHybridViewConfig.h
-    copy_and_fix_header SFHybridViewController.h
-    copy_and_fix_header SFIdentityCoordinator.h
-    copy_and_fix_header SFIdentityData.h
-    copy_and_fix_header SFLogger.h
-    copy_and_fix_header SFOAuthCoordinator.h
-    copy_and_fix_header SFOAuthCredentials.h
-    copy_and_fix_header SFOAuthInfo.h
-    copy_and_fix_header SFPushNotificationManager.h
-    copy_and_fix_header SFUserAccount.h
-    copy_and_fix_header SFUserAccountConstants.h
-    copy_and_fix_header SFUserAccountManager.h
-    echo "Copying needed libraries to src/ios/frameworks"
-    copy_lib ()
-    {
-        echo "* Copying $1"
-        find tmp -name $1 -exec cp {} src/ios/frameworks/ \;
-    }
-    copy_lib libSalesforceCommonUtils.a
-    copy_lib libSalesforceHybridSDK.a
-    copy_lib libSalesforceOAuth.a
-    copy_lib libSalesforceSDKCore.a
-    copy_lib libSalesforceSecurity.a
-    copy_lib libcrypto.a
-    copy_lib libsqlcipher.a
-    copy_lib libssl.a
-    echo "Copying Settings.bundle out of bower_components"
-    cp -r bower_components/mobilesdk-ios-package/Templates/NativeAppTemplate/__NativeTemplateAppName__/__NativeTemplateAppName__/Settings.bundle src/ios/resources/
-    echo "Copying SalesforceSDKResources.bundle out of bower_components"
-    cp -r bower_components/mobilesdk-ios-distribution/SalesforceSDKResources.bundle src/ios/resources/
-
-    echo "*** Shared ***"
-    echo "Copying split cordova.force.js out of bower_components"
-    cp bower_components/mobilesdk-shared/gen/plugins/com.salesforce/*.js www/
-
-    echo "*** Cleanup ***"
-    rm -rf bower_components
-    rm -rf tmp
+if [ ! -d "$IOS_SDK" ]
+then
+    echo "You must clone SalesforceMobileSDK-iOS next to SalesforceMobileSDK-CordovaPlugin"
+    exit 0
+else
+    echo "Found SalesforceMobileSDK-iOS"
+    echo "Building SalesforceMobileSDK-iOS"
+    cd $IOS_SDK
+    ./install.sh
+    cd build
+    ant 
+    cd ../../SalesforceMobileSDK-CordovaPlugin
 fi
+    
+if [ ! -d "$ANDROID_SDK" ]
+then
+    echo "You must clone SalesforceMobileSDK-Android next to SalesforceMobileSDK-CordovaPlugin"
+    exit 0
+else
+    echo "Found SalesforceMobileSDK-Android"
+    cd $ANDROID_SDK
+    ./install.sh
+    cd ../SalesforceMobileSDK-CordovaPlugin
+fi
+
+if [ ! -d "$SHARED_SDK" ]
+then
+    echo "You must clone SalesforceMobileSDK-Shared next to SalesforceMobileSDK-CordovaPlugin"
+    exit 0
+else
+    echo "Found SalesforceMobileSDK-Shared"
+    cd $ANDROID_SDK
+    ./install.sh
+    cd ../SalesforceMobileSDK-CordovaPlugin
+fi
+
+echo "*** Creating directories ***"
+echo "Starting clean"
+rm -rf tmp src
+echo "Creating tmp directory"
+mkdir -p tmp
+echo "Creating android directories"
+mkdir -p src/android/native
+mkdir -p src/android/hybrid
+mkdir -p src/android/assets
+echo "Creating ios directories"
+mkdir -p src/ios/headers
+mkdir -p src/ios/frameworks
+mkdir -p src/ios/classes
+mkdir -p src/ios/resources
+
+echo "*** Android ***"
+echo "Copying SalesforceSDK library"
+cp -r $ANDROID_SDK/native/SalesforceSDK src/android/native/
+echo "Copying SmartStore library"
+cp -r $ANDROID_SDK/hybrid/SmartStore src/android/hybrid/
+echo "Copying icu461.zip"
+cp $ANDROID_SDK/external/sqlcipher/assets/icudt46l.zip src/android/assets/
+echo "Copying sqlcipher"
+cp -r $ANDROID_SDK/external/sqlcipher/libs/* src/android/hybrid/SmartStore/libs/    
+
+echo "*** iOS ***"
+echo "Copying SalesforceHybridSDK library"    
+unzip $IOS_SDK/build/artifacts/SalesforceHybridSDK-Debug.zip -d tmp
+echo "Copying SalesforceOAuth library"    
+unzip $IOS_SDK/build/artifacts/SalesforceOAuth-Debug.zip -d tmp
+echo "Copying SalesforceSDKCore library"    
+unzip $IOS_SDK/build/artifacts/SalesforceSDKCore-Debug.zip -d tmp
+echo "Copying SalesforceSecurity library"    
+unzip $IOS_SDK/build/artifacts/SalesforceSecurity-Debug.zip -d tmp
+echo "Copying SalesforceCommonUtils library"    
+cp -r $IOS_SDK/external/ThirdPartyDependencies/SalesforceCommonUtils  tmp
+echo "Copying openssl library"    
+cp -r $IOS_SDK/external/ThirdPartyDependencies/openssl  tmp
+echo "Copying sqlcipher library"    
+cp -r $IOS_SDK/external/ThirdPartyDependencies/sqlcipher  tmp
+echo "Copying AppDelegate"    
+cp $IOS_SDK/hybrid/SampleApps/VFConnector/VFConnector/Classes/AppDelegate.*  tmp
+echo "Copying and fixing needed headers to src/ios/headers"
+copy_and_fix SFAuthenticationManager.h headers
+copy_and_fix SFCommunityData.h headers
+copy_and_fix SFHybridViewConfig.h headers
+copy_and_fix SFHybridViewController.h headers
+copy_and_fix SFIdentityCoordinator.h headers
+copy_and_fix SFIdentityData.h headers
+copy_and_fix SFLogger.h headers
+copy_and_fix SFOAuthCoordinator.h headers
+copy_and_fix SFOAuthCredentials.h headers
+copy_and_fix SFOAuthInfo.h headers
+copy_and_fix SFPushNotificationManager.h headers
+copy_and_fix SFUserAccount.h headers
+copy_and_fix SFUserAccountConstants.h headers
+copy_and_fix SFUserAccountManager.h headers
+copy_and_fix AppDelegate.h classes
+copy_and_fix AppDelegate.m classes
+echo "Copying needed libraries to src/ios/frameworks"
+copy_lib libSalesforceCommonUtils.a
+copy_lib libSalesforceHybridSDK.a
+copy_lib libSalesforceOAuth.a
+copy_lib libSalesforceSDKCore.a
+copy_lib libSalesforceSecurity.a
+copy_lib libcrypto.a
+copy_lib libsqlcipher.a
+copy_lib libssl.a
+echo "Copying Images.xcassets"
+cp -r $IOS_SDK/hybrid/SampleApps/VFConnector/VFConnector/Images.xcassets src/ios/resources/
+echo "Copying Settings.bundle"
+cp -r $IOS_SDK/hybrid/SampleApps/VFConnector/VFConnector/Settings.bundle src/ios/resources/
+echo "Copying SalesforceSDKResources.bundle"
+cp -r $IOS_SDK/shared/resources/SalesforceSDKResources.bundle src/ios/resources/
+
+echo "*** Shared ***"
+echo "Copying split cordova.force.js out of bower_components"
+cp $SHARED_SDK/gen/plugins/com.salesforce/*.js www/
+
+echo "*** Cleanup ***"
+rm -rf tmp
 
 
