67909e367128ce2b9361dd65b571ae3df931c3e0
Wolfgang Mathurin <wmathurin@salesforce.com>
2014-05-30 14:06:33 -0700

    Postinstall compiles salesforcesdk and smartstore library projects - as a result cordova build now works

--- tools/postinstall-android.js
+++ tools/postinstall-android.js
@@ -79,7 +79,13 @@ exec('android update project -p . -t "android-' + targetAndroidApi + '" -l ' + l
 console.log('Updating SalesforceSDK to use cordovaLib');
 exec('android update project -p . -t "android-' + targetAndroidApi + '" -l ' + cordovaLibProject, {cwd: path.resolve(process.cwd(), 'plugins/com.salesforce/src/android/native/SalesforceSDK')});
 
+console.log('Building SalesforceSDK library');
+exec('ant clean debug', {cwd: path.resolve(process.cwd(), 'plugins/com.salesforce/src/android/native/SalesforceSDK')});
+
+
 if (useSmartStore) {
     console.log('Updating SmartStore library target android api');
     exec('android update project -p . -t "android-' + targetAndroidApi + '"', {cwd: path.resolve(process.cwd(), 'plugins/com.salesforce/src/android/hybrid/SmartStore')});
+    console.log('Building SmartStore library');
+    exec('ant clean debug', {cwd: path.resolve(process.cwd(), 'plugins/com.salesforce/src/android/hybrid/SmartStore')});
 }
